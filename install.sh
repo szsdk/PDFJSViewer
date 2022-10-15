d2ec8e1d0b356df7deb06496ebd670c5fa05422b
szsdk <shen_zhou@u.nus.edu>
2022-10-15 08:03:33 +0200

    fix: load cmaps

--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ PWD=`pwd`
 
 mkdir third
 wget 'https://github.com/mozilla/pdf.js/releases/download/v2.16.105/pdfjs-2.16.105-legacy-dist.zip' -P third
-unzip third/pdfjs-2.16.105-legacy-dist.zip -d ${dist}
+unzip -qq third/pdfjs-2.16.105-legacy-dist.zip -d ${dist}
 ln -s ${PWD}/pdfjs/py_control.js ${dist}/web/py_control.js
+
+# When loading CJK files, pdf.js needs extra cmaps files which cannot be easily fetched due to some 
+# browser security issues. Replace the relative cmaps URL with an online official cmaps URL.
+sed -i.bak 's/\.\.\/web\/cmaps\//https:\/\/mozilla.github.io\/pdf.js\/web\/cmaps\//g' ${dist}/web/viewer.js
 wget 'https://mozilla.github.io/pdf.js/images/logo.svg' -O ${dist}/logo.svg
+
 if command -v inkscape &> /dev/null
 then
     inkscape ${dist}/logo.svg -o ${dist}/logo.png -d 384
@@ -18,5 +23,5 @@ then
     echo "~/.config/PDPDFJSViewer.toml exists."
 else
     cp -n PDFJSViewer.toml ~/.config/
-    echo pdfjs = \"`pwd`/third/pdfjs-2.16.105-legacy-dist\" >> ~/.config/PDFJSViewer.toml
+    echo pdfjs = \"`pwd`/${dist}\" >> ~/.config/PDFJSViewer.toml
 fi
